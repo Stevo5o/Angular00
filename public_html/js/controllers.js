4bccc2b77c53a7e77080732acc7d623439d7b362
Stevo5o <stevo.joc@gmail.com>
2015-03-06 14:39:46 +0000

    add next prev buttons

--- public_html/js/controllers.js
+++ public_html/js/controllers.js
@@ -22,6 +22,18 @@
             $http.get('js/data.json').success(function (data) {
                 $scope.characters = data;
                 $scope.whichItem = $routeParams.itemId;
+
+                if($routeParams.itemId > 0) {
+                	$scope.prevItem = Number($routeParams.itemId)-1;
+                } else {
+                	$scope.prevItem = $scope.characters.length-1;
+                } 
+
+                if($routeParams.itemId < $scope.characters.length-1) {
+                	$scope.nextItem = Number($routeParams.itemId)+1;
+                } else {
+                	$scope.nextItem = 0;
+                }
             });
         }]);
 }()); // end immediately invoked anonymous function
